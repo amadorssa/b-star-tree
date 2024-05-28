e9e8392eaed1e7facb6bb632291af664e8dff66b
Harico04 <FaustoMedina004@gmail.com>
2024-05-27 23:30:25 -0700

    arreglo de archivos

---
+++ BStarTree.cpp
@@ -0,0 +1,130 @@
+#include "BStarTree.hpp"
+
+template<typename T>
+BStarTree<T>::BStarTree(int o): root(nullptr), numNodes(0),order(o) {};
+/********************************************************/
+template<typename T>
+BStarTree<T>::~BStarTree()
+{
+    empty();
+}
+/********************************************************/
+template<typename T>
+BStarTree<T>::BStarTree(const BStarTree<T>& t): numNodes(0), root(nullptr)
+{
+    *this=t;
+}
+/********************************************************/
+template<typename T>
+BStarTree<T>& BStarTree<T>::operator=(const BStarTree<T>& t)
+{
+
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::add(T v)
+{
+    add(v,root);
+}
+/********************************************************/
+template<typename T>
+bool BStarTree<T>::isEmpty() const
+{
+    return root==nullptr;
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::empty()
+{
+    empty(root);
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::Delete(T v)
+{
+    Delete(v,root);
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::print() const
+{
+    print(root);
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::printBackwards() const
+{
+    printBackwards(root);
+}
+
+/********************************************************/
+template<typename T>
+void BStarTree<T>::printByLevels() const
+{
+    
+}
+
+
+
+/********************************************************/
+//PRIVATE METHODS
+/********************************************************/
+
+
+
+
+/********************************************************/
+template<typename T>
+void BStarTree<T>::add(const T& v, Node*& subRoot)
+{
+
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::print(Node *subRoot) const
+{
+
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::printBackwards(Node *subRoot) const
+{
+
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::empty(Node *& subRoot)
+{
+
+}
+/********************************************************/
+template<typename T>
+void BStarTree<T>::Delete(T v, Node*& subRoot)
+{
+
+}
+
+
+
+
+/********************************************************/
+//NODE METHODS
+/********************************************************/
+
+
+
+/********************************************************/
+template<typename T>
+BStarTree<T>::Node::Node(OrderedList<T> vals,DoubleLinkedList<Node*> s): values(vals),children(s){}
+/********************************************************/
+template<typename T>
+BStarTree<T>::Node::Node(T v,DoubleLinkedList<Node*> s): children(s)
+{
+    values.add(v);
+}
+/********************************************************/
+template<typename T>
+bool BStarTree<T>::Node::isLeaf() const
+{
+    return children.isEmpty();
+}
---
+++ BStarTree.hpp
@@ -0,0 +1,52 @@
+#ifndef B_STAR_TREE_HPP
+#define B_STAR_TREE_HPP
+
+#include "OrderedList.hpp"
+
+template<typename T>
+class BStarTree
+{
+
+    public:
+
+        BStarTree(int o);
+        ~BStarTree();
+        BStarTree(const BStarTree& t);
+        BStarTree& operator=(const BStarTree& t);
+
+        void add(T v);
+        void Delete(T v);
+        bool isEmpty() const;
+        void empty();
+        void print() const;
+        void printBackwards() const;
+        void printByLevels() const;
+
+    private://attributes
+
+    int order;
+    struct Node
+    {   
+        Node(OrderedList<T> vals,DoubleLinkedList<Node*> s);
+        Node(T v,DoubleLinkedList<Node*> s);
+        OrderedList<T> values;
+        DoubleLinkedList<Node*> children;
+        bool isLeaf() const;
+        bool hasLeftSon() const;
+        bool hasRightSon() const;
+
+    };
+    Node *root;
+    int numNodes;
+
+    private://methods
+
+        void add(const T& v, Node*& subRoot);
+        void print(Node* subRoot) const;
+        void printBackwards(Node* subRoot) const;
+        void empty(Node*& subRoot);
+        void Delete(T v, Node*& subRoot);
+
+};
+
+#endif//B_STAR_TREE_HPP
